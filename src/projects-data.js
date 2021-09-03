aeaa6f9e01dff867e5075ec2c34fa3451a2edcee
Shay Doron <shayd2110@gmail.com>
2021-09-03 11:47:06 +0300

    Readme Updated

--- src/projects-data.js
+++ src/projects-data.js
@@ -1,37 +1,34 @@
 export const projects = [
-  {
-    title: "React Reserve",
-    subtitle: "MERN Stack",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-1.gif",
-    link: "https://reactbootcamp.com",
-  },
-  {
-    title: "React Tracks",
-    subtitle: "React and Python",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-2.gif",
-    link: "https://reactbootcamp.com",
-  },
-  {
-    title: "DevChat",
-    subtitle: "React and Firebase",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-3.gif",
-    link: "https://reactbootcamp.com",
-  },
-  {
-    title: "Epic Todo App",
-    subtitle: "React Hooks",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-4.gif",
-    link: "https://reactbootcamp.com",
-  },
+	{
+		title: "Movie Search",
+		subtitle: "React App",
+		description:
+			"Search and save movie by search box, Fetch data from OMDb API",
+		image: "./movie-app.gif",
+		link: "https://shay-doron-movie-app.netlify.app/",
+	},
+	{
+		title: "React Tracks",
+		subtitle: "React and Python",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+		image: "./project-2.gif",
+		link: "https://reactbootcamp.com",
+	},
+	{
+		title: "DevChat",
+		subtitle: "React and Firebase",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+		image: "./project-3.gif",
+		link: "https://reactbootcamp.com",
+	},
+	{
+		title: "Epic Todo App",
+		subtitle: "React Hooks",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+		image: "./project-4.gif",
+		link: "https://reactbootcamp.com",
+	},
 ];
-
-
-
