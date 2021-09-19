eb1dfae3d73192af64442f81926e06ea729c0387
Shay Doron <shayd2110@gmail.com>
2021-09-19 11:30:13 +0300

    fix navbar

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ function About() {
 				<div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
 					<h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
 						Hi, I'm <strong>Shay Doron</strong>
-						<br className="hidden lg:inline-block" />
+						<br className="block lg:inline-block sm:inline-block" />
 						I'm a Softwear & Fullstack dev and I love to build
 						amazing apps
 					</h1>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ArrowRightIcon } from "@heroicons/react/solid";
+import classNames from "classnames";
 
 const Navbar = () => {
+	const [isShrunk, setShrunk] = useState(false);
+	useEffect(() => {
+		const onScroll = () => {
+			setShrunk((isShrunk) => {
+				if (
+					!isShrunk &&
+					(document.body.scrollTop > 20 ||
+						document.documentElement.scrollTop > 20)
+				) {
+					return true;
+				}
+
+				if (
+					isShrunk &&
+					document.body.scrollTop < 4 &&
+					document.documentElement.scrollTop < 4
+				) {
+					return false;
+				}
+
+				return isShrunk;
+			});
+		};
+
+		window.addEventListener("scroll", onScroll);
+		return () => window.removeEventListener("scroll", onScroll);
+	}, []);
+
+	const containerClasses = [];
+	if (isShrunk) {
+		containerClasses.push(
+			"container mx-auto flex flex-row  p-5 justify-center   items-center"
+		);
+	} else {
+		containerClasses.push(
+			"container mx-auto flex flex-wrap p-5  justify-center  flex-col md:flex-row items-center"
+		);
+	}
 	return (
-		<header className="bg-gray-800 md:sticky sm:sticky top-0 z-10">
-			<div className="container mx-auto flex flex-wrap p-5  flex-col md:flex-row items-center">
-				<a
-					href=" "
-					className="title-font font-medium text-white mb-4 md:mb-0"
-				>
-					<a href="#about" className="ml-3 text-lg">
+		<header
+			className={classNames(
+				"bg-gray-800 sticky  bg-opacity-80 border-b border-transparent transition  duration-500 dark:bg-black dark:text-white top-0 z-10",
+				{
+					"border-gray-200  dark:border-gray-800 backdrop-blur     ":
+						isShrunk,
+				}
+			)}
+		>
+			<div className={containerClasses.join(" ")}>
+				<a href=" " className="title-font font-medium text-white  ">
+					<a
+						href="#about"
+						className=" mr-1 md:mr-2 sm:mr-2 sm:ml-0 md:ml-0 text-lg "
+					>
 						Shay Doron
 					</a>
 				</a>
@@ -26,7 +73,7 @@ const Navbar = () => {
 				</nav>
 				<a
 					href="#contact"
-					className="inline-flex items-stretch  bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
+					className="inline-flex items-stretch  border-0 py-1 px-3 place-self- focus:outline-none hover:bg-gray-700 rounded text-base  md:mt-0"
 				>
 					Work with Me
 					<ArrowRightIcon className="w-4 h-4 ml-1 self-center" />
